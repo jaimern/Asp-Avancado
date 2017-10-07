141a4d0a63e0cf80c0fe6646eeb52465c8e49e98
jaimern <31370141+jaimern@users.noreply.github.com>
2017-10-07 17:38:18 -0300

    Initial commit

---
+++ Empresa.Dominio/Contato.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Empresa.Dominio
+{
+    public class Contato
+    {
+
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Assunto { get; set; }
+        [Required]
+        public string Mensagem { get; set; }
+
+    }
+}
---
+++ Empresa.Mvc/Controllers/ContatosController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Empresa.Repositorios.SqlServer;
+using Empresa.Dominio;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Empresa.Mvc.Controllers
+{
+    public class ContatosController : Controller
+    {
+        private readonly EmpresaDbContext _contexto;
+        public ContatosController(EmpresaDbContext contexto)
+        {
+            _contexto = contexto;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(_contexto.Contatos);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Contato contato)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(contato);
+            }
+            _contexto.Add(contato);
+            _contexto.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
