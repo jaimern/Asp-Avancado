ba5a3015a389c6a17e2c50eaca56d1033300ebbc
jaimern <jaime.rn@globo.com>
2017-10-21 17:49:26 -0200

    atualizado

--- Loja.Mvc/Scripts/Leiloes/Details.js
+++ Loja.Mvc/Scripts/Leiloes/Details.js
@@ -1,3 +1,35 @@
 ﻿//Kyle Simpson
 //object literal
-var Details = {};
+var Details = {
+    produtoId: 0,
+    nomeParticipante: "",
+
+    inicializar: function (produtoId) {
+        this.produtoId = produtoId;
+        this.coectarLeilaoHub();
+        this.vinculareventos();
+    },
+
+    coectarLeilaoHub: function () {
+
+        var connection = $.hubConnection();
+        var hub = connection.createHubProxy("LeilaoHub");
+
+        connection.start();
+    },
+
+    vinculareventos: function () {
+        $("#entratButton").on("click", function () {
+            this.entrarLeilao();
+        });
+    },
+
+    entrarLeilao: function(){
+        this.nomeParticipante = $("#nomeParticipante".val());
+        //this.leilaoHub.invoke("Participar", this.nomeParticipante, this.produtoId);
+        $("#participanteDiv").hide();
+        $("#lanceDiv").show();
+        $("#valorLance").focus();
+}
+
+};
